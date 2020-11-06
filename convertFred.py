0c08c031c10624936dff6a5e12794fc9c4c5414c
Thomas1601 <thomas.metsue@viacesi.fr>
2020-11-06 16:44:46 +0100

    updated 11-2020

--- convertFred.py
+++ convertFred.py
@@ -17,12 +17,12 @@ path = "./input/content"
 dirs = os.listdir( path )
 # This would print all the files and directories
 for file in dirs:
-    bertrand = file
-    gilbert = bertrand.replace('md', 'html')
-    #print(gilbert)
-    with open("./input/content/"+bertrand, "r", encoding="utf-8") as input_file:
+    toto = file
+    tata = toto.replace('md', 'html')
+    #print(tata)
+    with open("./input/content/"+toto, "r", encoding="utf-8") as input_file:
         md = markdown(input_file.read())
-    with open("./output/"+gilbert, "x", encoding="utf-8") as output_file:
+    with open("./output/"+tata, "x", encoding="utf-8") as output_file:
         temp = Environment(loader=FileSystemLoader(searchpath='./')).get_template("template.html")
         output_file.write(temp.render(content = md))
     
